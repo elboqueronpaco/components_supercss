da0ee7cc72b79617bdf59a86c054e9a84630dfee
Francisco Javier Moreno García <elboqueronpaco@gmail.com>
2018-10-31 11:51:34 +0100

    components con vanilla js

--- src/vanilla.js
+++ src/vanilla.js
@@ -2,21 +2,21 @@ import style from './style.css'
 import main from './scss/main.scss'
 import logo from './img/logo-DPscss.svg'
 import data from './data.json'
-import hola from './hola'
-console.log('Hola mundo Webpack, sin configuración');
-console.log(hola);
+import { HelloWorld } from './components/App'
+
 
 const d = document,
     app = d.getElementById('app'),
     h1 = d.createElement('h1'),
     img = d.createElement('img'),
     p = d.createElement('p'),
-    nav = d.createElement('nav')
+    nav = d.createElement('nav'),
+    hello = new HelloWorld('Vanilla JS')
 
 let menu = ''
 data.links.forEach(link => menu += `<a class="Menu-link" href="${link[1]}">${link[0]}</a>` );
-h1.textContent = 'Webpack'
-p.textContent = 'Creando un frameword de css y js con Webpack'
+h1.textContent = 'Webpack + Vanilla'
+p.textContent = hello.greet()
 img.src = logo
 nav.classList.add('Menu')
 nav.innerHTML = menu
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,25 @@
-const HtmlWebpackPlugin = require ('html-webpack-plugin')
+const HtmlWebpackPlugin = require ('html-webpack-plugin'),
+    MiniCssExtracyPlugin = require('mini-css-extract-plugin'),
+    CleanWebpackPlugin = require('clean-webpack-plugin'),
+    autoprefixer = require('autoprefixer'),
+    {VueLoaderPlugin} = require ('vue-loader')
 module.exports = {
+    entry:{
+        js: './src/index.js',
+        vanilla: './src/vanilla.js',
+        react: './src/app.js',
+        vue: './src/vue.js',
+        ts: './src/ts.js'
+    } ,
+    output: {
+        filename: '[name].[chunkhash].js'
+    },
+    devtool: 'source-map',
     module:{
         rules:[
             //objeto para babel
             {
-                test: /\.(js)$/,
+                test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
                 use:{
                  loader: 'babel-loader'
@@ -19,14 +34,91 @@ module.exports = {
                     options: { mininize: true }
                     }
                 ]
+            },
+            // Hojas de estilos
+            {
+                test: /\.(css|scss|sass)$/,
+                use: [
+                    'style-loader',
+                    MiniCssExtracyPlugin.loader,
+                    'css-loader?minimize&sourceMap',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            autoprefixer: {
+                                browser: [ 'last 2 versions' ]
+                            },
+                            sourceMap: true,
+                            plugins: () => [autoprefixer]
+                        }
+                    },
+                    'sass-loader?outputStyle=compressed&sourceMap'
+                ]
+            },
+            // Archivo de imagenes
+            {
+                test: /\.(jpe?g|png|gif|svg|webp)$/i,
+                use:[
+                    'file-loader?name=assets/[name].[ext]',
+                    'image-webpack-loader?bypassOnDebug'
+                ]
+            },
+            // Archivo 
+            {
+                test: /\.(ttf|eot|woff2?|mp4|mp3|txt|xml|pdf)$/i,
+                use:[
+                    'file-loader?name=assets/[name].[ext]'
+                ]
+            },
+            //Para vue
+            {
+                test: /\.vue$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'vue-loader'
+                }
+            },
+            // Para ts
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'ts-loader'
+                }
             }
-
         ]
     },
     plugins: [
+        new CleanWebpackPlugin([ 'dist/**/*.*']),
+        new MiniCssExtracyPlugin({
+            filename: '[name].[chunkhash].css',
+            chunkFilename: '[id].css'
+        }),
+        new VueLoaderPlugin(),
         new HtmlWebpackPlugin({
             template: './src/template.html',
-            filename: './index.html'
+            filename: 'index.html',
+            chunks: ['js']
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/template.html',
+            filename: 'vanilla.html',
+            chunks: ['vanilla']
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/template.html',
+            filename: 'react.html',
+            chunks: ['react']
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/template.html',
+            filename: 'vue.html',
+            chunks: ['vue']
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/template.html',
+            filename: 'ts.html',
+            chunks: ['ts']
         })
     ]
 }
